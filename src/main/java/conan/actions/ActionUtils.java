3b256557c45da7be5210bd13b0fa4f5f0f4d0de4
Evgeniy Koveshnikov <e.koveshnikov@cft.ru>
2019-12-25 18:11:26 +0700

    added cleaning repository before conan install

--- src/main/java/conan/actions/ActionUtils.java
+++ src/main/java/conan/actions/ActionUtils.java
@@ -1,23 +1,31 @@
 package conan.actions;
 
 import com.google.common.collect.Lists;
+import com.intellij.notification.NotificationType;
 import com.intellij.openapi.components.ServiceManager;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.project.Project;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.StringUtils;
+
+import java.awt.Component;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
 import conan.commands.BuildConfigure;
 import conan.commands.Install;
-import conan.commands.IsInstalledCommand;
+import conan.commands.task.AsyncConanTask;
 import conan.persistency.settings.ConanProjectSettings;
 import conan.profiles.CMakeProfile;
 import conan.profiles.ConanProfile;
 import conan.ui.ConanToolWindow;
 import conan.ui.profileMatching.ProfileMatcher;
-import org.apache.commons.lang.StringUtils;
 
-import java.awt.*;
-import java.util.List;
-import java.util.Map;
+import static conan.utils.Utils.log;
 
 /**
  * Created by Yahav Itzhak on Feb 2018.
@@ -31,6 +39,9 @@ class ActionUtils {
      * @param component the {@link ProfileMatcher} dialog will be shown in this component position.
      * @param update    true if it's update and install action.
      */
+
+    private static final Logger logger = Logger.getInstance(AsyncConanTask.class);
+
     static void runInstall(Project project, Component component, boolean update) {
         if (!ConanToolWindow.isConanInstalled(project)){
             return;
@@ -44,8 +55,20 @@ class ActionUtils {
             matchProfilesAndInstall(project, component, update);
             return;
         }
-        cMakeProfiles.forEach(cMakeProfile -> new Install(project, cMakeProfile, conanProfile, update).run_async(conanProfile, null, null));
+        cMakeProfiles.forEach(cMakeProfile ->{
+            String cleanedDirectory = cMakeProfile.getTargetDir();
+            try {
+                FileUtils.cleanDirectory(new File(cleanedDirectory));
+                log(logger, "Clean directory: " + cleanedDirectory, "", NotificationType.INFORMATION);
+            } catch (IOException e) {
+                log(logger, "Can`t clean directory: " + cleanedDirectory, "", NotificationType.ERROR);
+                e.printStackTrace();
+            }
+            new Install(project, cMakeProfile, conanProfile, update).run_async(conanProfile, null, null);
+        });
     }
+
+
 
     static void runBuildConfigure(Project project, Component component) {
         if (!ConanToolWindow.isConanInstalled(project)){
